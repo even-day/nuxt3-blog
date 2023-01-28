f6bc1aea70f6357adfd2456905a1125f33152e53
hasee <even_day@outlook.com>
2023-01-28 12:12:55 +0800

    添加评论功能

--- config.ts
+++ config.ts
@@ -7,24 +7,24 @@ export default {
 
   /** ⇊⇊⇊⇊⇊⇊  可选修改  ⇊⇊⇊⇊⇊⇊ */
   // --- 下面是强烈建议修改的 ---
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  domain: "https://blog.yunyuyuan.net", // rss域名
-  SEO_title: " - yunyuyuan blog", // 搜索引擎显示的标题
-  SEO_description: "云与原的博客，书写值得书写的东西", // description meta header
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客", // keywords meta header
+  title: "even days", // 网站标题
+  nickName: "偶数日", // 昵称
+  domain: "https://www.eaea.ml", // rss域名
+  SEO_title: " - evendays blog", // 搜索引擎显示的标题
+  SEO_description: "偶数日的博客，书写值得书写的东西", // description meta header
+  SEO_keywords: "evendays,evendays's blog,偶数日,偶数日的博客", // keywords meta header
   // --- 下面是需要额外设置的 ---
   CloudflareAnalyze: "", // cloudflare的统计，https://developers.cloudflare.com/analytics/web-analytics
-  CommentRepoId: "", // https://giscus.app/zh-CN
-  CommentDiscussionCategoryId: "", // https://giscus.app/zh-CN
+  CommentRepoId: "R_kgDOI2fiXQ", // https://giscus.app/zh-CN
+  CommentDiscussionCategoryId: "DIC_kwDOI2fiXc4CT2VV", // https://giscus.app/zh-CN
   Comment: {
-    articles: false,
+    articles: true,
     records: false,
     knowledges: false
   }, // 是否开启评论，请先设置上面的 CommentRepoId 和 CommentDiscussionCategoryId
   MongoDb: {
     enabled: true, // 是否开启mongodb(浏览量统计)，请先设置 https://vercel.com/integrations/mongodbatlas
-    initialVisitors: 1, // 如果设置成10000，那么发一篇文章立马就有10000个浏览量！
+    initialVisitors: 100, // 如果设置成10000，那么发一篇文章立马就有10000个浏览量！
     visitFromOwner: false // 网站拥有者访问时，是否增加浏览量
   },
   // --- 下面是可改可不改的 ----
